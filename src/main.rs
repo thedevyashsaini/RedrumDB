ee5d5e6906e46718f53842c2045869d1a5c966bf
Devyash Saini <dysaini2004@gmail.com>
2026-03-15 23:23:33 +0530

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,17 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener: TcpListener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write_all(b"+PONG\r\n").unwrap();
-            }
-            Err(e) => {
-                println!("error: {}", e);
+
+    loop {
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut _stream) => {
+                    println!("accepted new connection");
+                    _stream.write_all(b"+PONG\r\n").unwrap();
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
             }
         }
     }
